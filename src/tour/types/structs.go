ac294670d4b3c4b0153f7cbe2653e7dce6e20d1e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-03-30 23:15:51 +0300

    chore(structs): additional example on structs

--- src/tour/types/structs.go
+++ src/tour/types/structs.go
@@ -1,6 +1,20 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Bootcamp struct {
+	// latitude of the event
+	Lat float64
+
+	// Longitude of the event
+	Lon float64
+
+	// Date of event
+	Date time.Time
+}
 
 type Vertex struct {
 	X int
@@ -9,6 +23,13 @@ type Vertex struct {
 
 func main() {
 	v := Vertex{1, 2}
+	bootamp := Bootcamp{
+		Lat: 34.012836,
+		Lon:  -118.495338,
+		Date: time.Now(),
+	}
+
+	fmt.Println(bootamp)
 
 	// assign a field in a struct
 	v.X = 9
