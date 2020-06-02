51ce668aead962957cd06a780dc4773ce315f93b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-06-02 21:41:01 +0300

    feat(algorithms): accumulate
    solution implementation

--- algorithms/accumulate/accumulate.go
+++ algorithms/accumulate/accumulate.go
@@ -1,1 +1,9 @@
 package accumulate
+
+// Accumulate takes in a collection & applies function to each item in collection
+func Accumulate(collection []string, function func(string) string)(output []string) {
+	for _, item := range collection {
+		output = append(output, function(item))
+	}
+	return output
+}
