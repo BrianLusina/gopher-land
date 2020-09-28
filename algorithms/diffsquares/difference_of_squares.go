2d17a260d61ebb1b5b6b1d494ada9fb1dff6b2b2
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-09-28 09:17:58 +0300

    refactor(algorithms): update diff squares

--- algorithms/diffsquares/difference_of_squares.go
+++ algorithms/diffsquares/difference_of_squares.go
@@ -1,31 +1,17 @@
 package diffsquares
 
-import (
-	"math"
-)
-
 // SquareOfSum returns the square of the sum of N natural numbers
 func SquareOfSum(n int) int {
-	total := 0
-	for i := 1; i <= n; i++ {
-		total += i
-	}
-	return int(math.Pow(float64(total), 2))
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
 
 // SumOfSquares returns the sum of the squares of the first N natural numbers
 func SumOfSquares(n int) int {
-	total := 0
-	for i := 1; i <= n; i++ {
-		total += int(math.Pow(float64(i), 2))
-	}
-	return total
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference returns the difference between the square of the sum of the first N natural numbers & sum of the squares of the first N natural numbers
 func Difference(n int) int {
-	sqOfSum := SquareOfSum(n)
-	sumOfSq := SumOfSquares(n)
-
-	return sqOfSum - sumOfSq
+	return SquareOfSum(n) - SumOfSquares(n)
 }
