a5c5d3a9073b5929a874d8d058656f12009872d8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-01-26 18:58:07 +0300

    feat(algorithms): largest series product solution

---
+++ algorithms/largest-series-product/largest_series_product.go
@@ -0,0 +1,41 @@
+package lsproduct
+
+import (
+	"fmt"
+)
+
+// LargestSeriesProduct takes in a string of digits and the span to get the largest product from a contiguous
+// series
+func LargestSeriesProduct(digits string, span int) (product int64, err error) {
+	if span < 0 {
+		return 0, fmt.Errorf("span is negative: %d", span)
+	}
+
+	if len(digits) < span {
+		return 0, fmt.Errorf("len(%s) < span: %d < %d", digits, len(digits), span)
+	}
+
+	digitsArray := make([]int64, len(digits))
+
+	for index, digit := range digits {
+		if digit < '0' || digit > '9' {
+			return 0, fmt.Errorf("input %q contains non-digits", digits)
+		}
+
+		digitsArray[index] = int64(digit - '0')
+	}
+
+	for index, last := 0, len(digitsArray)-span+1; index < last; index++ {
+		currentProduct := int64(1)
+
+		for _, digit := range digitsArray[index : index+span] {
+			currentProduct *= digit
+		}
+
+		if currentProduct > product {
+			product = currentProduct
+		}
+	}
+
+	return
+}
