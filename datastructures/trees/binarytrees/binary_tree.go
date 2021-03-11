fda004ff238df3c05969716fac4cbe19236e2b6f
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-11 14:37:27 +0300

    feat(binarytrees): in order traversal

--- datastructures/trees/binarytrees/binary_tree.go
+++ datastructures/trees/binarytrees/binary_tree.go
@@ -14,3 +14,69 @@ func (t *BinaryTreeNode) NewBinaryTree(data int) BinaryTreeNode {
 		Data: data,
 	}
 }
+
+func (t *BinaryTreeNode) inorderTraversalIteravely() (result []int) {
+	stack := []*BinaryTreeNode{}
+	current := t
+
+	for current != nil || len(stack) != 0 {
+		for current != nil {
+			stack = append(stack, current)
+			current = current.Left
+		}
+
+		current, stack = stack[len(stack)-1], stack[:len(stack)-1]
+		result = append(result, current.Data)
+		current = current.Right
+	}
+	return
+}
+
+func (t *BinaryTreeNode) inorderTraversalRecurse(root *BinaryTreeNode) (result []int) {
+	if root != nil {
+		if root.Left != nil {
+			t.inorderTraversalRecurse(t.Left)
+		}
+
+		result = append(result, root.Data)
+
+		if root.Right != nil {
+			t.inorderTraversalRecurse(t.Right)
+		}
+	}
+
+	return
+}
+
+func (t *BinaryTreeNode) inOrderMorrisTraversal() (result []int) {
+	current := t
+	var pre *BinaryTreeNode
+
+	for current != nil {
+		if current.Left == nil {
+			// add the current value of the node
+			result = append(result, current.Data)
+			// # Move to next right node
+			current = current.Right
+		} else {
+			// # we have a left subtree
+			pre = current.Left
+
+			// # find rightmost
+			for pre.Right != nil {
+				pre = pre.Right
+			}
+
+			// # put current after the pre node
+			pre.Right = current
+			// # store current node
+			temp := current
+			// # move current to top of new tree
+			current = current.Left
+			// # original current left be None, avoid infinite loops
+			temp.Left = nil
+		}
+	}
+
+	return
+}
