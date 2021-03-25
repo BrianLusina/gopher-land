fdb1d85cd746c314112fb26ad610325f3781c5a5
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-25 22:27:05 +0300

    feat(linkedlist): singly linked list

--- datastructures/lists/linkedlist/singlylinkedlist/singlylinkedlist.go
+++ datastructures/lists/linkedlist/singlylinkedlist/singlylinkedlist.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "fmt"
+
 // SinglyLinkedListNode of a Singly Linked List
 type SinglyLinkedListNode struct {
 	Data interface{}
@@ -18,6 +20,78 @@ func CreateList() *SinglyLinkedList {
 // NewNode creates a new LinkedList Node
 func NewNode(data interface{}) *SinglyLinkedListNode {
 	return &SinglyLinkedListNode{data, nil}
+}
+
+// Prepend adds a new node to the beggining of the list
+func (ll *SinglyLinkedList) Prepend(val interface{}) {
+	node := NewNode(val)
+	node.Next = ll.Head
+	ll.Head = node
+}
+
+// Append adds a new Node at the end of a linked list
+func (ll *SinglyLinkedList) Append(val interface{}) {
+	node := NewNode(val)
+
+	if ll.Head == nil {
+		ll.Head = node
+		return
+	}
+
+	current := ll.Head
+	for ; current.Next != nil; current = current.Next {
+	}
+
+	current.Next = node
+}
+
+// DeleteAtBeg removes a node at the beggining of a linked list & returns its value.
+// Returns -1 if the list is empty
+func (ll *SinglyLinkedList) DeleteAtBeg() interface{} {
+	if ll.Head == nil {
+		return -1
+	}
+
+	current := ll.Head
+	ll.Head = current.Next
+
+	return current.Data
+}
+
+// DeleteAtEnd removes a node at the end of a linked list & returns its value.
+// Returns -1 if the list is empty
+func (ll *SinglyLinkedList) DeleteAtEnd() interface{} {
+	if ll.Head == nil {
+		return -1
+	}
+
+	current := ll.Head
+	for ; current.Next.Next != nil; current = current.Next {
+	}
+
+	data := current.Next.Data
+	current.Next = nil
+	return data
+}
+
+// Count counts the number of nodes in a Linked List
+func (ll *SinglyLinkedList) Count() (count int) {
+	current := ll.Head
+
+	for current != nil {
+		count++
+	}
+
+	return
+}
+
+// Display prints out the elements of the list.
+func (ll *SinglyLinkedList) Display() {
+	for cur := ll.Head; cur != nil; cur = cur.Next {
+		fmt.Print(cur.Data, " ")
+	}
+
+	fmt.Print("\n")
 }
 
 // IsPalindrome
