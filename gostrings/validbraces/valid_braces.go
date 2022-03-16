caf91b931099f97e27b29c775e32ba739a8b4391
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-03-16 08:39:00 +0300

    feat(strings): valid braces

---
+++ gostrings/validbraces/valid_braces.go
@@ -0,0 +1,33 @@
+package validbraces
+
+var openersToClosersMap = map[rune]rune{
+	'(': ')',
+	'{': '}',
+	'[': ']',
+	'<': '>',
+}
+
+var openers = map[rune]bool{'(': true, '{': true, '[': true, '<': true}
+var closers = map[rune]bool{')': true, '}': true, ']': true, '>': true}
+
+func ValidBraces(str string) bool {
+	stack := []rune{}
+
+	for _, char := range str {
+		if openers[char] {
+			stack = append(stack, char)
+		} else if closers[char] {
+			if len(stack) == 0 {
+				return false
+			} else {
+				lastOpener := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				if openersToClosersMap[lastOpener] != char {
+					return false
+				}
+			}
+		}
+	}
+	return len(stack) == 0
+}
---
+++ gostrings/validbraces/valid_braces_test.go
@@ -0,0 +1,143 @@
+package validbraces
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidBraces(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ValidBraces Test Suite")
+}
+
+var _ = Describe("ValidBraces", func() {
+	It("should return true for (){}[]", func() {
+		input := "(){}[]"
+		expected := true
+		actual := ValidBraces(input)
+		assert.Equal(GinkgoT(), expected, actual)
+	})
+
+	It("should return true for ([{}])", func() {
+		input := "([{}])"
+		expected := true
+		actual := ValidBraces(input)
+		assert.Equal(GinkgoT(), expected, actual)
+	})
+
+	It("should return false for (}", func() {
+		input := "(}"
+		expected := false
+		actual := ValidBraces(input)
+		assert.Equal(GinkgoT(), expected, actual)
+	})
+
+	It("should return false for [(])", func() {
+		input := "[(])"
+		expected := false
+		actual := ValidBraces(input)
+		assert.Equal(GinkgoT(), expected, actual)
+	})
+
+	It("should return false for [({)](]", func() {
+		input := "[({)](]"
+		expected := false
+		actual := ValidBraces(input)
+		assert.Equal(GinkgoT(), expected, actual)
+	})
+
+	It("should return true for {}()[]", func() {
+		input := "{}()[]"
+		expected := true
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return true for ([{}])", func() {
+		input := "([{}])"
+		expected := true
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return false for ([{)]}", func() {
+		input := "([{)]}"
+		expected := false
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return false for ([}{])", func() {
+		input := "([}{])"
+		expected := false
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return true for {}({})[]", func() {
+		input := "{}({})[]"
+		expected := true
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return true for (({{[[]]}}))", func() {
+		input := "(({{[[]]}}))"
+		expected := true
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return false for (((({{}})[]))", func() {
+		input := "(((({{}})[]))"
+		expected := false
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return true for )(}{[]", func() {
+		input := ")(}{[]"
+		expected := false
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	It("should return true for (({[]{()}}[{(())}[]]))", func() {
+		input := "(({[]{()}}[{(())}[]]))"
+		expected := true
+		actual := ValidBraces(input)
+		Expect(actual).To(Equal(expected))
+	})
+
+	parens := []string{"()", "[]", "{}"}
+
+	rng := rand.New(rand.NewSource(rand.Int63()))
+
+	for i := 0; i < 100; i++ {
+		str := parens[rng.Int31n(3)]
+
+		for i := rng.Int31n(10); i > 0; i-- {
+			index := rng.Int31n(int32(len(str)))
+			str = str[:index] + parens[rng.Int31n(3)] + str[index:]
+		}
+
+		valid := true
+		//flip a coin - valid or invalid?
+		if rng.Int31n(2) == 1 {
+			//tails, time to invalidate this bitch!
+			valid = false
+			index := rng.Int31n(int32(len(str)))
+			str = str[:index] + str[index+1:]
+		}
+
+		It(fmt.Sprintf("Should return %v for %s", valid, str), func() {
+			actual := ValidBraces(str)
+			assert.Equal(GinkgoT(), valid, actual)
+		})
+	}
+})
