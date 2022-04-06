59ea65cc3e4e2aacf124386fcd6b629670859233
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-04-06 07:44:41 +0300

    feat(basics): goroutines

---
+++ basics/goroutines/using_wait_group.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func HeavyFunction1(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Do a lot of stuff
+	time.Sleep(5 * 1e9) // sleep for 5 seconds
+	println("Done with heavy function 1")
+}
+
+func HeavyFunction2(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Do a lot of stuff
+	time.Sleep(2 * 1e9) // sleep for 2 seconds
+	println("Done with heavy function 2")
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go HeavyFunction1(wg)
+	go HeavyFunction2(wg)
+	wg.Wait()
+	fmt.Printf("All Finished!")
+}
