9122b31fe93854435d35d9bf5fbc514d2840eb1e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-04-07 07:43:20 +0300

    feat(basics): channels & goroutines

---
+++ basics/goroutines/block_goroutine.go
@@ -0,0 +1,30 @@
+// Illustrates where the goroutine pump sends integers in an infinite loop on the channel. However, because there is
+//no receiver, the only output is the number 0
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	go pump(ch1)       // pump hangs
+	fmt.Println(<-ch1) // prints only 0
+	time.Sleep(1e9)
+}
+
+// suck only receives integers from the channel ch
+func suck(ch <-chan int) {
+	// use a for-range loop to read from a channel
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
+
+// pump sends integers to the channel ch
+func pump(ch chan<- int) {
+	for i := 0; ; i++ {
+		ch <- i
+	}
+}
