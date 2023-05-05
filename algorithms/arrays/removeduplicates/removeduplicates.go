69106e920e133059ddb6ffee43f1ddbdcd916e44
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-05-05 09:37:33 +0300

    doc(algorithms): add remove duplicates doc

--- algorithms/arrays/removeduplicates/removeduplicates.go
+++ algorithms/arrays/removeduplicates/removeduplicates.go
@@ -1,5 +1,32 @@
 package removeduplicates
 
+/**
+RemoveDuplicates removes duplicates from a sorted slice of integers.
+
+Removes duplicates from a sorted list of integers and returns the number of the unique elements in the list. This
+modifies the list in place so no extra space is used, effectively the Space complexity is O(1). However, the time
+complexity is O(n) where n is the length of the list.
+
+This uses 2 pointers to keep track of the unique and non-unique elements in the list of integers. i pointer is at
+the element before and j pointer is at the next element. These are moved along the elements in the list as below:
+
+1. as long as j is less than the length of the list we shall iterate until j reaches the end of the list, i.e. the
+last element in the list
+
+2. if the element at position i is not equal to the element at position j(at this point js is ahead of i), we
+increase the i index by 1 and assign the index at i the value of the index at j and increment the index at j, i.e.
+moving the pointer j and i forward by 1
+
+3. if the elements are equal, then that means we have encountered a duplicate & therefore we move the pointer j
+forward by 1 until we encounter a unique element.
+
+4. When the loop is complete we return i +1. This is because the pointer i is now pointing to the last unique
+element in the list.
+
+The other elements after the pointer i are the duplicate elements. This modifies the sorted list in place retaining
+"some" of the duplicated elements to the right of i. Note that this means we will not know how many times an
+element occurred in the original sorted list of elements.
+**/
 func RemoveDuplicates(nums []int) int {
 	if len(nums) < 2 {
 		return len(nums)
