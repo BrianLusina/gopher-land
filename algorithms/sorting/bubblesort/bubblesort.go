0c2d028695ab4f00f9c4df566b2152976c9f92d3
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-05-09 15:59:30 +0300

    feat(algorithms): bubble sort

--- algorithms/sorting/bubblesort/bubblesort.go
+++ algorithms/sorting/bubblesort/bubblesort.go
@@ -4,18 +4,21 @@ import "gopherland/pkg/types"
 
 // BubbleSort sorts a slice of numbers in ascending order using bubble sort algorithm
 // Approach
-// 	select the first element of the array
-// 	compare it with its next element
+//
+//	select the first element of the array
+//	compare it with its next element
 //	if it is larger than the next element then swap them
 //	else do nothing
 //	keep doing this for every index of the array
 //	repeat the above process n times.
-//Time Complexity
+//
+// Time Complexity
+//
 //	O(n^2) Worst case performance
 //	O(n) Best-case performance
 //	O(n^2) Average performance
 //	Space Complexity
-//	O(1) Worst case
+//	O(1) Worst case as no extra memory is used & the original slice is sorted.
 func BubbleSort[T types.Number](arr []T) []T {
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-i-1; j++ {
@@ -23,6 +26,23 @@ func BubbleSort[T types.Number](arr []T) []T {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
+	}
+	return arr
+}
+
+func bubbleSort(arr []int) []int {
+	unsortedUntilIdx := len(arr) - 1
+	sorted := false
+
+	for !sorted {
+		sorted = true
+		for i := 0; i < unsortedUntilIdx; i++ {
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				sorted = false
+			}
+		}
+		unsortedUntilIdx--
 	}
 	return arr
 }
--- algorithms/sorting/bubblesort/bubblesort_test.go
+++ algorithms/sorting/bubblesort/bubblesort_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBubbleSort(t *testing.T) {
@@ -27,3 +28,53 @@ var _ = Describe("BubbleSort", func() {
 		Expect(actual).To(Equal(expected))
 	})
 })
+
+type testCase struct {
+	input    []int
+	expected []int
+}
+
+var testCases = []testCase{
+	{
+		input:    []int{3, 4, 1, 8, 3, 5, 3, 5},
+		expected: []int{1, 3, 3, 3, 4, 5, 5, 8},
+	},
+}
+
+func TestBubbleSort1(t *testing.T) {
+	for _, tc := range testCases {
+		actual := BubbleSort(tc.input)
+		assert.Equal(t, tc.expected, actual)
+	}
+}
+
+func TestBubbleSort2(t *testing.T) {
+	for _, tc := range testCases {
+		actual := bubbleSort(tc.input)
+		assert.Equal(t, tc.expected, actual)
+	}
+}
+
+func BenchmarkBubbleSort1(b *testing.B) {
+	if testing.Short() {
+		b.Skip()
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testCases {
+			BubbleSort(tc.input)
+		}
+	}
+}
+
+func BenchmarkBubbleSort2(b *testing.B) {
+	if testing.Short() {
+		b.Skip()
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testCases {
+			bubbleSort(tc.input)
+		}
+	}
+}
