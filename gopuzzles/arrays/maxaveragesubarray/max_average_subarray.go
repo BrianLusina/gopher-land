8366eb4b7e3a19f653dfd1aab95be72b2a21e656
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-06-30 08:56:21 +0300

    chore(docs): puzzles - code comments max average

--- gopuzzles/arrays/maxaveragesubarray/max_average_subarray.go
+++ gopuzzles/arrays/maxaveragesubarray/max_average_subarray.go
@@ -2,7 +2,23 @@ package maxaveragesubarray
 
 import "gopherland/math/utils"
 
-// findMaxAverage finds the maximum average in sub array of length k
+/*
+findMaxAverage finds the maximum average in sub array of length k
+Finds the maximum average in a sub array of length k from the provided list/array.
+
+Uses a sliding window to find the maximum average.
+This traverses over nums just once, and on the go keep determining the sums possible for the subarrays of length k.
+To understand the idea, assume that we already know the sum of elements from index i to index i+k, say it is
+current_sum.
+
+Now, to determine the sum of elements from the index i+1 to the index i+k+1, all we need to do is to subtract the
+element nums[i] from x and to add the element nums[i+k+1] to current_sum. We can carry out our process based on this
+idea and determine the maximum possible average.
+
+Complexity Analysis:
+- Time Complexity: O(n). Iterate over the given nums array of length n only once
+- Space Complexity: O(1). Constant extra space is used
+*/
 func findMaxAverage(nums []int, k int) float64 {
 	if len(nums) <= 1 || len(nums) == k {
 		return float64(utils.Sum(nums)) / float64(k)
