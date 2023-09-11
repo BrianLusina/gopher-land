ec9406da2591d2556206488a82166fefcd5ab549
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-09-11 10:04:08 +0300

    feat(puzzles): search - square root estimate

---
+++ gopuzzles/search/binarysearch/sqrtestimate/sqrt_estimate.go
@@ -0,0 +1,27 @@
+package sqrtestimate
+
+func sqrt_estimate(x int) int {
+	if x == 0 {
+		return 0
+	}
+
+	left := 1
+	right := x
+	result := 0
+
+	for left <= right {
+		mid := left + (right-left)/2
+		square := mid * mid
+
+		if square == x {
+			return mid
+		} else if square < x {
+			result = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return result
+}
---
+++ gopuzzles/search/binarysearch/sqrtestimate/sqrt_estimate_test.go
@@ -0,0 +1,64 @@
+package sqrtestimate
+
+import (
+	"fmt"
+	"gopherland/pkg/utils"
+	"math"
+	"testing"
+)
+
+type testCase struct {
+	n        int
+	expected int
+}
+
+func generateTests() []testCase {
+	start := 0
+	ceil := int(math.Pow(2, 10))
+	numbers := utils.Range(start, ceil, 1)
+
+	testCases := []testCase{}
+
+	for _, num := range numbers {
+		expected := int(math.Floor(math.Sqrt(float64(num))))
+
+		testCase := testCase{
+			n:        num,
+			expected: expected,
+		}
+
+		testCases = append(testCases, testCase)
+	}
+
+	return testCases
+}
+
+func TestSqrtEstimate(t *testing.T) {
+	for _, tc := range generateTests() {
+		t.Run(fmt.Sprintf("sqrtEstimate(%d)", tc.n), func(t *testing.T) {
+			actual := sqrt_estimate(tc.n)
+
+			if actual != tc.expected {
+				t.Fatalf("expected %d, got %d", tc.expected, actual)
+			}
+		})
+	}
+}
+
+func BenchmarkSqrtEstimate(b *testing.B) {
+	if testing.Short() {
+		b.Skip()
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tc := range generateTests() {
+			b.Run(fmt.Sprintf("sqrtEstimate(%d)", tc.n), func(b *testing.B) {
+				actual := sqrt_estimate(tc.n)
+
+				if actual != tc.expected {
+					b.Fatalf("expected %d, got %d", tc.expected, actual)
+				}
+			})
+		}
+	}
+}
