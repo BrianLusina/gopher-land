77e5673781a037f31ec73825a818fb6b3bc36066
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-11-12 10:11:15 +0300

    feat(basics-io): demo of file sync

--- basics/io/files/write_to_file.go
+++ basics/io/files/write_to_file.go
@@ -6,6 +6,24 @@ import (
 	"os"
 )
 
+// write_to_file writes content of a slice of bytes to a given file name
+// this demonstrates that the closure of the file is deferred and the error ignored this is because
+// this ensure the file buffer is written to disk with f.Sync() which could potentially return an error.
+// NOTE: closing the file after a successful write does not guarantee that the file is persisted to disk, hence the use of f.Sync()
+func write_to_file(filename string, content []byte) error {
+	f, err := os.Open(filename)
+
+	defer func() {
+		_ = f.Close()
+	}()
+	_, err = f.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return f.Sync()
+}
+
 func run_write_to_file() {
 	outputFile, outputError := os.OpenFile("./files/output.txt", os.O_WRONLY|os.O_CREATE, 0666)
 	if outputError != nil {
