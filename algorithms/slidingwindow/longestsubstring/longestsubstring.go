81d2131c622edfb910dbd99b0349b11d7cefc594
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-03-18 08:50:23 +0300

    chore(algorithms): go lint

--- algorithms/slidingwindow/longestsubstring/longestsubstring.go
+++ algorithms/slidingwindow/longestsubstring/longestsubstring.go
@@ -1,28 +1,31 @@
 package longestsubstring
 
-import "math"
+import (
+	"gopherland/pkg/utils"
+	"math"
+)
 
 func lengthOfLongestSubstring(s string) int {
-    start := 0
-    result := 0    
-    seen := map[uint8]int{}
-    
-    for end := 0; end < len(s); end++ {
-        char := s[end]
-        
-        duplicateIndex, ok:= seen[char]
-        if ok {
-            result = int(math.Max(float64(result), float64(end-start)))
-            for i:=start;i <duplicateIndex; i++ {
-                delete(seen, s[i])
-            }
-            
-            start = duplicateIndex + 1
-        }
-        
-        seen[s[end]] = end
-    }
-    
-    result = int(math.Max(float64(result), float64(len(s) - start)))
-    return result
+	start := 0
+	result := 0
+	seen := map[uint8]int{}
+
+	for end := 0; end < len(s); end++ {
+		char := s[end]
+
+		duplicateIndex, ok := seen[char]
+		if ok {
+			result = int(math.Max(float64(result), float64(end-start)))
+			for i := start; i < duplicateIndex; i++ {
+				delete(seen, s[i])
+			}
+
+			start = duplicateIndex + 1
+		}
+
+		seen[s[end]] = end
+	}
+
+	result = utils.Max(result, len(s)-start)
+	return result
 }
