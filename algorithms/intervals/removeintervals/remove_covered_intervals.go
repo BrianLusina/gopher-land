cd193fca40078c5a4449e4b0819f4740feb31fba
Lusina <12752833+BrianLusina@users.noreply.github.com>
2026-03-07 13:04:06 +0300

    refactor(algorithms, intervals): remove covered intervals - copy intervals input

--- algorithms/intervals/removeintervals/remove_covered_intervals.go
+++ algorithms/intervals/removeintervals/remove_covered_intervals.go
@@ -11,21 +11,25 @@ func RemoveCoveredIntervals(intervals [][]int) int {
 		return 0
 	}
 
+	// create a copy of the intervals being passed in to avoid side effects on the caller side. This will incur a space complexity cost of O(n)
+	sorted := make([][]int, len(intervals))
+	copy(sorted, intervals)
+
 	// Sort by start time in ascending order and then by end time in descending order
-	sort.Slice(intervals, func(i, j int) bool {
-		if intervals[i][0] != intervals[j][0] {
+	sort.Slice(sorted, func(i, j int) bool {
+		if sorted[i][0] != sorted[j][0] {
 			// Sort by start time ascending
-			return intervals[i][0] < intervals[j][0]
+			return sorted[i][0] < sorted[j][0]
 		}
 
 		// If start times are equal, sort by end time descending
-		return intervals[i][1] > intervals[j][1]
+		return sorted[i][1] > sorted[j][1]
 	})
 
 	maxEndSeen := -math.MaxInt
 	count := 0
 
-	for _, interval := range intervals {
+	for _, interval := range sorted {
 		current_end := interval[1]
 		if current_end > maxEndSeen {
 			count += 1
