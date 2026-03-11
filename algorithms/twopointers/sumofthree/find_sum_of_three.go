2ba812c9419ec67fde66a0f8fae7c540b2403da9
Lusina <12752833+BrianLusina@users.noreply.github.com>
2026-03-11 14:58:09 +0300

    feat(algorithms, two pointers): three number sum

--- algorithms/twopointers/sumofthree/find_sum_of_three.go
+++ algorithms/twopointers/sumofthree/find_sum_of_three.go
@@ -1,6 +1,8 @@
 package sumofthree
 
-import "sort"
+import (
+	"sort"
+)
 
 func findSomeOfThree(numbers []int, target int) bool {
 	sort.IntSlice(numbers).Sort()
@@ -20,4 +22,51 @@ func findSomeOfThree(numbers []int, target int) bool {
 	}
 
 	return false
+}
+
+func threeNumberSum(nums []int, targetSum int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{}
+	}
+	n := len(nums)
+	numbers := make([]int, n)
+	copy(numbers, nums)
+	sort.IntSlice(numbers).Sort()
+	result := [][]int{}
+
+	for idx := range n {
+		num := numbers[idx]
+
+		// Skip duplicate values to avoid duplicate triplets
+		if idx > 0 && num == numbers[idx-1] {
+			continue
+		}
+
+		left, right := idx+1, n-1
+
+		for left < right {
+			totalSum := num + numbers[left] + numbers[right]
+
+			if totalSum == targetSum {
+				result = append(result, []int{num, numbers[left], numbers[right]})
+				// move the left pointer to avoid duplicates while it is still less than the right
+				for left < right && numbers[left] == numbers[left+1] {
+					left += 1
+				}
+
+				for left < right && numbers[right] == numbers[right-1] {
+					right -= 1
+				}
+
+				left += 1
+				right -= 1
+			} else if totalSum > targetSum {
+				right -= 1
+			} else {
+				left += 1
+			}
+		}
+	}
+
+	return result
 }
--- algorithms/twopointers/sumofthree/find_sum_of_three_test.go
+++ algorithms/twopointers/sumofthree/find_sum_of_three_test.go
@@ -3,6 +3,8 @@ package sumofthree
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type testCase struct {
@@ -38,12 +40,49 @@ func BenchmarkFindSumOfThree(b *testing.B) {
 
 	for _, tc := range testCases {
 		for i := 0; i < b.N; i++ {
-			b.Run(fmt.Sprintf("findSumOfThree(%v, %d)", tc.numbers, tc.numbers), func(b *testing.B) {
+			b.Run(fmt.Sprintf("findSumOfThree(%v, %d)", tc.numbers, tc.target), func(b *testing.B) {
 				result := findSomeOfThree(tc.numbers, tc.target)
 				if result != tc.expected {
 					b.Errorf("For input %v, expected %v but got %v", tc.numbers, tc.expected, result)
 				}
 			})
+		}
+	}
+}
+
+type testCaseThreeNumSum struct {
+	testCase
+	expected [][]int
+}
+
+var testCasesThreeNumSum = []testCaseThreeNumSum{
+	{
+		testCase{
+			[]int{12, 3, 1, 2, -6, 5, -8, 6},
+			0,
+			true,
+		},
+		[][]int{{-8, 2, 6}, {-8, 3, 5}, {-6, 1, 5}}},
+}
+
+func TestThreeNumSum(t *testing.T) {
+	for _, tc := range testCasesThreeNumSum {
+		t.Run(fmt.Sprintf("threeNumberSum(%v, %d)", tc.numbers, tc.target), func(t *testing.T) {
+			result := threeNumberSum(tc.numbers, tc.target)
+			// assert.ElementsMatch(t, result, tc.expected)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func BenchmarkThreeNumSum(b *testing.B) {
+	if testing.Short() {
+		b.Skip("Skipping benchmark in short mode.")
+	}
+
+	for _, tc := range testCasesThreeNumSum {
+		for b.Loop() {
+			threeNumberSum(tc.numbers, tc.target)
 		}
 	}
 }
